839342b0d1a1ddd3e8246ca204bbb9d757366466
Victor Fernandez <manuel1508vc@gmail.com>
2023-12-28 00:12:56 -0500

    otros cambios

--- SEMANA10/dia03-react-invoice-app/src/App.jsx
+++ SEMANA10/dia03-react-invoice-app/src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react"
+import { fetchInvoices } from "./services/invoices"
+
 export default function App() {
+  const [invoices, setInvoices] = useState([])
+
+  useEffect(() =>{
+    //console.log('Me ejecuto por primera vez')
+
+   
+
+    fetchInvoices()
+    .then((invoices) => setInvoices(invoices))
+  },[])
+
   return (
+<>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    {JSON.stringify(invoices)}
+
+</>
   )
 }
